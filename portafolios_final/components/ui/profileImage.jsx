472c66b62e0318b2e431208c8c7edddee4dbacb0
wang-franco <frnco1029@gmail.com>
2024-08-17 16:54:26 -0600

    Portafolios Final

---
+++ portafolios_final/components/ui/profileImage.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Page() {
+  return (
+    <Image
+      src="https://avatars.githubusercontent.com/u/75237417?s=400&u=18bac40bc911802e0f5f7a6115cd5441c9a0d097&v=4"
+      width={500}
+      height={500}
+      alt="Picture of the author"
+    />
+  )
+}
